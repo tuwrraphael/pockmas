fd3ab46a7f523498d2d28d7b7587b5fe9ffe4194
tuwrraphael <raphael.hauk@racing.tuwien.ac.at>
2021-10-07 14:36:22 +0200

    allocator savepoints & memory growth

--- raptor/bump_allocator.c
+++ raptor/bump_allocator.c
@@ -1,9 +1,36 @@
+#include <stddef.h>
+#include "bump_allocator.h"
+
+#ifdef WASM_BUILD
+
 extern unsigned char __heap_base;
 
 unsigned char *bump_pointer = &__heap_base;
+unsigned char *savepoint;
 
-void *allocate(int number_of_bytes) {
+void *malloc(size_t number_of_bytes)
+{
   unsigned int r = (unsigned int)bump_pointer;
   bump_pointer += number_of_bytes;
+  size_t memory_size = __builtin_wasm_memory_size(0) * 65536;
+  if (memory_size - (r - __heap_base) < number_of_bytes)
+  {
+    __builtin_wasm_memory_grow(0, 1 + number_of_bytes / 65536);
+  }
   return (void *)r;
+}
+
+#endif
+
+void create_savepoint(void)
+{
+#ifdef WASM_BUILD
+  savepoint = bump_pointer;
+#endif
+}
+void reset_to_savepoint(void)
+{
+#ifdef WASM_BUILD
+  bump_pointer = savepoint;
+#endif
 }
--- raptor/bump_allocator.h
+++ raptor/bump_allocator.h
@@ -1,6 +1,14 @@
 #ifndef BUMP_ALLOCATOR_H
 #define BUMP_ALLOCATOR_H
+#include <stddef.h>
 
-void *allocate(int numerOfBytes);
+#ifdef WASM_BUILD
+void *malloc(size_t number_of_bytes);
+#else
+#include <malloc.h>
+#endif
+
+void create_savepoint(void);
+void reset_to_savepoint(void);
 
 #endif
