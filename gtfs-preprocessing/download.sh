e383f121f7c4c4eae8a73dd3c9c81cb20b7f4e3d
tuwrraphael <raphael.hauk@racing.tuwien.ac.at>
2021-10-13 13:22:57 +0200

    build wasm

--- gtfs-preprocessing/download.sh
+++ gtfs-preprocessing/download.sh
@@ -1,3 +1,3 @@
-wget http://www.wienerlinien.at/ogd_realtime/doku/ogd/gtfs/gtfs.zip -O gtfs.zip
+wget http://www.wienerlinien.at/ogd_realtime/doku/ogd/gtfs/gtfs.zip -q -O gtfs.zip
 mkdir -p gtfs
 unzip gtfs.zip -d gtfs
