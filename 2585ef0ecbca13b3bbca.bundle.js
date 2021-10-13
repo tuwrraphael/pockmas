2d7ddcac58f865c853f1dd4c9664a59350cfa337
tuwrraphael <raphael.hauk@racing.tuwien.ac.at>
2021-10-13 12:01:21 +0000

    Deploying to gh-pages from  @ 14bf3183bbbb42c773d64836268b17b8e9857eb8 🚀

---
+++ 2585ef0ecbca13b3bbca.bundle.js
@@ -0,0 +1,2 @@
+!function(){"use strict";"serviceWorker"in navigator&&window.addEventListener("load",(async()=>{navigator.serviceWorker.register("./sw.js").then((e=>{})).catch((e=>{console.log("SW registration failed: ",e)}))}))}();
+//# sourceMappingURL=2585ef0ecbca13b3bbca.bundle.js.map
