90e6364762ae377f0ce0b2e1dc441cfef9c9845a
tuwrraphael <raphael.hauk@racing.tuwien.ac.at>
2021-12-06 22:26:45 +0100

    binary stopsearch optimization

--- stopsearch/stopsearch.c
+++ stopsearch/stopsearch.c
@@ -5,7 +5,7 @@ static stopsearch_index_t stopsearch_index;
 
 void *stopsearch_allocate(uint32_t num_nodes, uint32_t num_children, uint32_t num_results)
 {
-	unsigned char* memory = malloc(num_nodes * sizeof(trie_node_t) + num_children * sizeof(children_lookup_t) + num_children * sizeof(children_index_t) + num_results * sizeof(trie_node_result_t));
+	unsigned char *memory = malloc(num_nodes * sizeof(trie_node_t) + num_children * sizeof(children_lookup_t) + num_children * sizeof(children_index_t) + num_results * sizeof(trie_node_result_t));
 	stopsearch_index.nodes = (void *)memory;
 	stopsearch_index.children_lookup = (void *)(memory + num_nodes * sizeof(trie_node_t));
 	stopsearch_index.children_index = (void *)(memory + num_children * sizeof(children_lookup_t) + num_nodes * sizeof(trie_node_t));
@@ -32,18 +32,25 @@ stopsearch_result_t *stopsearch_step(uint8_t letter)
 {
 	if (current_node->num_children > 0)
 	{
-		for (uint32_t children_index = current_node->children_index; children_index < current_node->children_index + current_node->num_children; children_index++)
+		uint32_t lower = current_node->children_index;
+		uint32_t upper = lower + current_node->num_children;
+		while (lower < upper)
 		{
-			if (stopsearch_index.children_lookup[children_index].letter == letter)
+			uint32_t mid = (lower + upper) / 2;
+			if (stopsearch_index.children_lookup[mid].letter == letter)
 			{
-				current_node = &stopsearch_index.nodes[stopsearch_index.children_index[children_index].index];
+				current_node = &stopsearch_index.nodes[stopsearch_index.children_index[mid].index];
 				result->num_results = current_node->num_results;
 				result->results = &stopsearch_index.results[current_node->results_index];
-				break;
+				return result;
 			}
-			else if (stopsearch_index.children_lookup[children_index].letter > letter)
+			else if (letter > stopsearch_index.children_lookup[mid].letter)
 			{
-				break;
+				lower = mid + 1;
+			}
+			else
+			{
+				upper = mid;
 			}
 		}
 	}
